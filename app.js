8fe32fdacd38ff46d29c27ff8264a66513575b2a
razrahul <razrahuleee.com>
2025-04-11 17:12:50 +0530

    stage & swagger-imp

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //root file (server ceated here)
 const express = require("express");
-require("dotenv").config();
+// require("dotenv").config();
 const cors = require("cors");
 const sequelize = require("./src/config/dbConnect");
 const Roles = require("./src/models/role");
@@ -10,7 +10,48 @@ const indexRouter = require("./src/routes/index");
 const { tableSync } = require("./src/utils/commonMethod");
 require('./src/associations');
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
+
+//swagger
+const fs = require("fs");
+const { fileURLToPath } = require("url");
+const path = require("path"); 
+const  YAML = require("yaml");
+const swaggerUi = require("swagger-ui-express");
+
+const PORT = process.env.PORT || 3001;
+
+// console.log("__filename:", __filename);
+// console.log("__dirname:", __dirname);
+
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+const file = fs.readFileSync(path.resolve(__dirname, './swagger.yaml'), 'utf8');
+const swaggerDocument = YAML.parse(
+  file?.replace(
+    "- url: ${{server}}",
+    `- url: ${process.env.SWAGGER_HOST_URL || `http://localhost:${PORT}`}/api/v1`
+  )
+);
+// console.log("file",file);
+
+// console.log("swaggerDocument",swaggerDocument);
+
+
 const app = express();
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: { docExpansion: "none" },
+    customSiteTitle: "CMS - Contact User Dashboard API Docs",
+  })
+);
 
 //middleware
 app.use(express.json());
@@ -19,7 +60,7 @@ app.use(cors());
 app.use("/api/V1", indexRouter);
 
 //server listen
-const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, async () => {
   try {
     console.log(`Server is running on port ${PORT}`);
