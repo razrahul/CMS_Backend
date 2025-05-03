10a958dff970dda6dc4484b3f546b77bc9f0faf6
razrahul <razrahuleee.com>
2025-05-03 13:53:31 +0530

    week stats add

--- src/routes/stats.js
+++ src/routes/stats.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
-const { createVisitors, getTodayStats, getAllStats}  = require('../controller/stats/statsController');
+const { createVisitors, getTodayStats, getAllStats, getWeekStats}  = require('../controller/stats/statsController');
 
 const router = express.Router();
 
 router.post('/visit', createVisitors);
 
 router.get('/today', getTodayStats);
+
+//week stats
+router.get('/week', getWeekStats);
 
 router.get('/all', getAllStats);
 
